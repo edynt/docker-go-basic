0ecebfb73a01f1b1e8d6b1d7876ee60b17b64f37
Tri Nguyen <triminh.nguyen2098@gmail.com>
2025-09-02 21:52:01 +0700

    add lesson 10

--- practice/ls10/main.go
+++ practice/ls10/main.go
@@ -25,3 +25,109 @@
 // select để xử lý timeout.
 
 package main
+
+import (
+	"context"
+	"fmt"
+	"sync"
+	"time"
+)
+
+type Order struct {
+	ID     string
+	Amount int
+}
+
+var stock = 100
+var mu sync.Mutex
+
+// Giả lập thanh toán có timeout
+func processPayment(ctx context.Context, order Order) error {
+	ch := make(chan bool, 1)
+
+	fmt.Println("Start: Before goroutine processing payment for", order.ID)
+
+	go func() {
+		fmt.Println("Goroutine Processing payment for", order.ID)
+		// giả lập call API thanh toán mất 2s
+		time.Sleep(2 * time.Second)
+		ch <- true // báo thành công
+	}()
+
+	fmt.Println("Start: After goroutine processing payment for", order.ID)
+
+	select {
+	case <-ctx.Done():
+		return fmt.Errorf("payment timeout or canceled")
+	case <-ch:
+		fmt.Println("Payment success for", order.ID)
+		return nil // thành công
+	}
+}
+
+func checkout(order Order, emailJobs chan<- string) {
+	fmt.Println("Start function checkout")
+	// 1. Lock tồn kho
+	mu.Lock()
+	if stock < order.Amount {
+		mu.Unlock()
+		fmt.Println("❌ Not enough stock for", order.ID)
+		return
+	}
+	stock -= order.Amount
+	mu.Unlock()
+
+	// 2. Context timeout 3s cho thanh toán
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+	fmt.Println("Checkout :: call before call process payment")
+	err := processPayment(ctx, order)
+	fmt.Println("Checkout :: processPayment done")
+	if err != nil {
+		// rollback nếu fail
+		mu.Lock()
+		stock += order.Amount
+		mu.Unlock()
+		fmt.Println("⚠️ Payment failed for", order.ID, "-> rollback")
+		return
+	}
+
+	// 3. Nếu thành công -> gửi job email
+	fmt.Println("Call set message to emailJobs")
+	emailJobs <- fmt.Sprintf("📧 Order %s confirmed!", order.ID)
+	fmt.Println("✅ Checkout success for", order.ID)
+}
+
+func emailWorker(jobs <-chan string, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for job := range jobs {
+		fmt.Println("Sending email:", job)
+		time.Sleep(1 * time.Second) // giả lập gửi email
+	}
+}
+
+func main() {
+	emailJobs := make(chan string, 10)
+	var wg sync.WaitGroup
+
+	// tạo 1 worker xử lý email
+	wg.Add(1)
+	go emailWorker(emailJobs, &wg)
+
+	// tạo một số order để test
+	orders := []Order{
+		{ID: "ORDER_0001", Amount: 10},
+		{ID: "ORDER_0002", Amount: 50},
+		{ID: "ORDER_0003", Amount: 60}, // sẽ fail vì stock không đủ
+	}
+
+	for _, order := range orders {
+		checkout(order, emailJobs)
+	}
+
+	fmt.Println("Call close emailJobs")
+	close(emailJobs) // đóng channel sau khi không còn order
+	wg.Wait()
+
+	fmt.Println("📦 Final stock:", stock)
+}
